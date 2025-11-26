aab6c93f21117bae621e2d57507599eb267b62dd
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2025-11-25 20:53:17 -0600

    Started making the LED class with Nolan and Chase.

--- src/main/java/frc/robot/LED.java
+++ src/main/java/frc/robot/LED.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.CANdleConfiguration;
 import com.ctre.phoenix6.controls.ColorFlowAnimation;
@@ -13,28 +15,38 @@ import com.ctre.phoenix6.signals.RGBWColor;
 import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
 import com.ctre.phoenix6.signals.StripTypeValue;
 
+import edu.wpi.first.wpilibj.util.Color;
+
+enum colors{
+    kGreen,
+    kWhite,
+    kRed,
+    kViolet
+}
+
+
 public class LED {
-    /* Configure CANdle */
-        CANdleConfiguration cfg = new CANdleConfiguration();
-        /* set the LED strip type and brightness */
-        cfg.LED.StripType = StripTypeValue.GRB;
-        cfg.LED.BrightnessScalar = 0.5;
-        /* disable status LED when being controlled */
-        //cfg.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Disabled;
-        m_candle.getConfigurator().apply(cfg);
-        /* clear all previous animations */
-        for (int i = 0; i < 8; ++i) {
-            m_candle.setControl(new EmptyAnimation(i));
-        }
-        /* set the onboard LEDs to a solid color */
-        m_candle.setControl(new SolidColor(0, 3).withColor(kGreen));
-        m_candle.setControl(new SolidColor(4, 7).withColor(kWhite));
-        m_candle.setControl(
-          new RainbowAnimation(kSlot0StartIdx, kSlot0EndIdx).withSlot(0));
 
+  private static final RGBWColor kGreen = new RGBWColor(0, 217, 0, 0);
+  private static final RGBWColor kWhite = new RGBWColor(Color.kWhite).scaleBrightness(0.5);
+  private static final RGBWColor kViolet = RGBWColor.fromHSV(Degrees.of(270), 0.9, 0.8);
+  private static final RGBWColor kRed = RGBWColor.fromHex("#D9000000").orElseThrow();
 
+  private static final int kSlot0StartIdx = 0;
+  private static final int kSlot0EndIdx = 7;
 
+  private final CANdle m_candle = new CANdle(1, "rio");
+  
 
+    public LED(){
+     /* Configure CANdle */
+     CANdleConfiguration cfg = new CANdleConfiguration();
+     /* set the LED strip type and brightness */
+     cfg.LED.StripType = StripTypeValue.GRB;
+     cfg.LED.BrightnessScalar = 0.5;
+    }
 
-
+    public void setColor(int firstLED, int lastLED, colors color){
+        m_candle.setControl(new SolidColor(firstLED, lastLED).withColor(kGreen));
+    }
 }
