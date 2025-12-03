d8a7e844e0589723b63f34eac136afb57e50b73a
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2025-12-02 20:22:28 -0600

    Finished LED class
    We finished the LED class. We fixed bugs in our LED class.

--- src/main/java/frc/robot/LED.java
+++ src/main/java/frc/robot/LED.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.Degrees;
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.CANdleConfiguration;
 import com.ctre.phoenix6.controls.ColorFlowAnimation;
+import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.EmptyAnimation;
 import com.ctre.phoenix6.controls.FireAnimation;
 import com.ctre.phoenix6.controls.LarsonAnimation;
@@ -18,15 +19,20 @@ import com.ctre.phoenix6.signals.StripTypeValue;
 import edu.wpi.first.wpilibj.util.Color;
 
 enum colors{
-    kGreen,
-    kWhite,
-    kRed,
-    kViolet
+    eGreen,
+    eWhite,
+    eRed,
+    eViolet
+}
+enum animations{
+    ColorFlow,
+    Fire,
+    Larson,
+    Rainbow
 }
 
 
 public class LED {
-
   private static final RGBWColor kGreen = new RGBWColor(0, 217, 0, 0);
   private static final RGBWColor kWhite = new RGBWColor(Color.kWhite).scaleBrightness(0.5);
   private static final RGBWColor kViolet = RGBWColor.fromHSV(Degrees.of(270), 0.9, 0.8);
@@ -47,6 +53,20 @@ public class LED {
     }
 
     public void setColor(int firstLED, int lastLED, colors color){
-        m_candle.setControl(new SolidColor(firstLED, lastLED).withColor(kGreen));
+        if (color == colors.eGreen){
+            m_candle.setControl(new SolidColor(firstLED, lastLED).withColor(kGreen));
+        }
+        else if (color == colors.eRed){
+            m_candle.setControl(new SolidColor(firstLED, lastLED).withColor(kRed));
+        }
+        else if (color == colors.eViolet){
+            m_candle.setControl(new SolidColor(firstLED, lastLED).withColor(kViolet));
+        }
+        else {
+            m_candle.setControl(new SolidColor(firstLED, lastLED).withColor(kWhite));
+        }
+    }
+    public void setAnimation(ControlRequest animation){
+        m_candle.setControl(animation);
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.controls.ColorFlowAnimation;
+import com.ctre.phoenix6.controls.FireAnimation;
+import com.ctre.phoenix6.controls.LarsonAnimation;
+import com.ctre.phoenix6.controls.RainbowAnimation;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
@@ -15,7 +21,17 @@ public class RobotContainer {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    LED led = new LED();
+    m_DriverController.y().onTrue(new InstantCommand(() -> {led.setColor(0, 7, colors.eRed);}));
+    m_DriverController.x().onTrue(new InstantCommand(() -> {led.setColor(0, 7, colors.eViolet);}));
+    m_DriverController.b().onTrue(new InstantCommand(() -> {led.setColor(0, 7, colors.eGreen);}));
+    m_DriverController.a().onTrue(new InstantCommand(() -> {led.setColor(0, 7, colors.eWhite);}));
+    m_DriverController.povUp().onTrue(new InstantCommand(() -> {led.setAnimation(new ColorFlowAnimation(0, 7));}));
+    m_DriverController.povDown().onTrue(new InstantCommand(() -> {led.setAnimation(new FireAnimation(0, 7));}));
+    m_DriverController.povLeft().onTrue(new InstantCommand(() -> {led.setAnimation(new RainbowAnimation(0, 7));}));
+    m_DriverController.povRight().onTrue(new InstantCommand(() -> {led.setAnimation(new LarsonAnimation(0, 7));}));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
